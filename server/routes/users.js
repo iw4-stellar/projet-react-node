802baaf73ba16569f5fcd55619360e0649729a14
Rudro Khan <rudrokhan.pro@gmail.com>
2022-07-04 16:56:55 +0200

    feat: Add of Sequelize

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/").user;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
+
+router.get("/users", userController.find);
+router.post("/users", userController.create);
 
 module.exports = router;
